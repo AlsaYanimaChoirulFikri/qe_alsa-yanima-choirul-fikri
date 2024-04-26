0d45b37ca8d3b2f034e80175776d538bbdb34e07
AlsaYanimaChoirulFikri <choirulfikri125@mail.ugm.ac.id>
2024-04-27 00:34:01 +0700

    Submit Tugas

---
+++ Code_Competence_2/Code-Competence-2/src/test/java/starter/stepdefinitions/DeleteUserSteps.java
@@ -0,0 +1,23 @@
+package starter.stepdefinitions;
+
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+import net.thucydides.core.annotations.Steps;
+import starter.user.DeleteUser;
+
+public class DeleteUserSteps {
+
+    @Steps
+    DeleteUser deleteUser;
+
+    @Given("I set API endpoint for delete user")
+    public void setApiEndpoint() {
+        deleteUser.setApiEndpoint();
+    }
+
+    @When("I send request to delete user with valid ID")
+    public void sendDeleteUserRequest() {
+        deleteUser.sendDeleteUserRequest();
+    }
+}
---
+++ Code_Competence_2/Code-Competence-2/src/test/java/starter/stepdefinitions/LoginValidUsernameSteps.java
@@ -0,0 +1,30 @@
+package starter.stepdefinitions;
+
+import io.cucumber.java.en.And;
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+import net.thucydides.core.annotations.Steps;
+import starter.user.LoginValidUsername;
+
+public class LoginValidUsernameSteps {
+
+    @Steps
+    LoginValidUsername loginValidUsername;
+
+    @Given("I set API endpoint for login")
+    public void setApiEndpoint() {
+        loginValidUsername.setApiEndpoint();
+    }
+
+    @When("I send request to login with valid username {string} and valid password {string}")
+    public void sendLoginRequest(String username, String password) {
+        loginValidUsername.sendLoginRequest(username, password);
+    }
+
+    @And("I receive valid data for login")
+    public void receiveValidDataForLogin() {
+        loginValidUsername.receiveValidDataForLogin();
+    }
+
+}
